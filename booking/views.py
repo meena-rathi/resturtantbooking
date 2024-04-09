7054497a4a94a42a176fdaa540effcfcdc038d76
meena-rathi Maheshwari <meenakumari16@hotmail.com>
2024-04-09 07:55:58 +0000

    add dinner and lunch menu

--- booking/views.py
+++ booking/views.py
@@ -11,4 +11,25 @@ def reservation(request):
     return render(request, 'reservation.html')
 
 def menu(request):
-    return render(request, 'menu.html')
+    lunch_items = [
+        {"image": "image/lunch1.webp", "description": "Pasta with vegetables and spicy sauces 10"},
+        {"image": "image/lunch2.jpg", "description": "Pasta with vegetables and spicy sauces 10"},
+        {"image": "image/lunch3.jpg", "description": "Pasta with vegetables and spicy sauces 10"},
+        {"image": "image/lunch4.jpg", "description": "Pasta with vegetables and spicy sauces 10"},
+    ]
+    dinner_items = [
+        {"image": "image/dinner1.jpg", "description": "Pasta with vegetables and spicy sauces 10"},
+        {"image": "image/dinner2.webp", "description": "Pasta with vegetables and spicy sauces 10"},
+        {"image": "image/dinner3.jpg", "description": "Pasta with vegetables and spicy sauces 10"},
+        {"image": "image/dinner4.jpg", "description": "Pasta with vegetables and spicy sauces 10"},
+    ]
+
+    # Define dinner items
+
+
+    context = {
+        'lunch_items': lunch_items,
+        'dinner_items': dinner_items,
+   
+    }
+    return render(request, 'menu.html', context)
